bad206d71c796f68dec3dc377beb1fd8f40d9942
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2022-04-19 11:15:03 +0900

    Use lbt_forward Directly
    s.t. Re-importing OpenBLAS32 is not necessary when the user's already loaded some LP64 BLAS.

--- src/Pfapack77.jl
+++ src/Pfapack77.jl
@@ -2,15 +2,18 @@ module Pfapack77
 
 using Libdl
 using LinearAlgebra
-if Int == Int64
-    using OpenBLAS32
-end
+using OpenBLAS32_jll
 using Pfapack_jll: libpfapack
 
 global libp77 = C_NULL
 global ilaenv = C_NULL
 
 __init__() = begin
+    # Ensure 32-bit BLAS APIS are loaded.
+    if (o -> o.interface != :lp64).(BLAS.lbt_get_config().loaded_libs) |> all
+        BLAS.lbt_forward(OpenBLAS32_jll.libopenblas)
+    end
+
     global libp77 = dlopen(libpfapack)
     global ilaenv = dlsym(libp77, :ilaenv_)
 end
